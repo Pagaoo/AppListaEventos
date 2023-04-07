82bc7a6e31986a1b2ded712474b3c810d8f43841
Pagão <gabrielpaganinibarroso@hotmail.com>
2023-04-07 17:35:56 -0300

    criando classe Person e seus atributos

--- app/src/main/java/devandroid/gabriel/applistanomes/view/MainActivity.java
+++ app/src/main/java/devandroid/gabriel/applistanomes/view/MainActivity.java
@@ -1,16 +1,21 @@
 package devandroid.gabriel.applistanomes.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import devandroid.gabriel.applistanomes.R;
+import model.Person;
 
 public class MainActivity extends AppCompatActivity {
+    Person person;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        person = new Person();
+
     }
 }
---
+++ app/src/main/java/model/Person.java
@@ -0,0 +1,44 @@
+package model;
+
+public class Person {
+
+    private String firstName;
+    private String surname;
+    private String listEvent;
+    private Double telephone;
+
+    public Person() {
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getListEvent() {
+        return listEvent;
+    }
+
+    public void setListEvent(String listEvent) {
+        this.listEvent = listEvent;
+    }
+
+    public Double getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(Double telephone) {
+        this.telephone = telephone;
+    }
+}

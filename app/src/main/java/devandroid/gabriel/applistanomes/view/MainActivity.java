bbe2ca749ab9eeec0c5fc5aac51acf23b40f54d9
Pagão <gabrielpaganinibarroso@hotmail.com>
2023-08-24 18:20:57 -0300

    scroll view with linear layout

--- app/src/main/java/devandroid/gabriel/applistanomes/view/MainActivity.java
+++ app/src/main/java/devandroid/gabriel/applistanomes/view/MainActivity.java
@@ -35,7 +35,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_spinner);
 
         personController = new PersonController(MainActivity.this);
         person = new Person();
